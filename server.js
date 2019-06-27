97c82c34a5075b00aa501f024bfa8e6a2c7f6fa6
raechel <odom.raechel@gmail.com>
2019-06-26 21:18:41 -0600

    two main get endpoints add

---
+++ server.js
@@ -0,0 +1,32 @@
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port} 🚀`);
+});
+
+app.get('/api/v1/crimes', (req, res) => {
+  database('crimes').select()
+    .then(crimes => {
+      res.status(200).json(crimes);
+    })
+    .catch(error => {
+      res.status(500).json({error});
+  });
+});
+
+app.get('/api/v1/neighborhoods', (req, res) => {
+  database('neighborhood').select()
+    .then(neighborhood => {
+      res.status(200).json(neighborhood);
+    })
+    .catch(error => {
+      res.status(500).json({error});       
+    });
+});
