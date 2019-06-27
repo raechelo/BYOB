b648dbdb239a3b5dd898bc16b5f33612a3b0c5aa
raechel <odom.raechel@gmail.com>
2019-06-27 10:36:24 -0600

    data rewritten and reseeded for proper key and foreign key matching

---
+++ murderData.js
@@ -0,0 +1,857 @@
+const murderData = [
+  {
+    name: 'Alhambra',
+    population: 85396,
+    murders: [
+      {
+        name: 'Lana Clarkson',
+        year: '2003',
+        location: '1700 Grand View Drive'
+      }
+    ]
+  },
+  {
+    name: 'Bakersfield',
+    population: 380874,
+    murders: [
+      {
+        name: 'The Onion Field Murder',
+        year: '1963',
+        location: 'Field off Route 99'
+      },
+    ]
+  },
+  {
+    name: 'Baldwin Hills',
+    population: 4871,
+    murders: [
+      {
+        name: 'Three Little Inglewood Girls',
+        year: '1937',
+        location: 'Baldwin Hills'
+      }
+    ]
+  },
+  {
+    name: 'Benedict Canyon, Los Angeles',
+    population: 3792621,
+    murders: [
+      {
+        name: 'Susan Berman',
+        year: '2000',
+        location: '1527 Benedict Canyon Road'
+      }
+    ]
+  },
+  {
+    name: 'Beverly Grove',
+    population: 22860,
+    murders: [
+      {
+        name: 'Rebecca Schaeffer',
+        year: '1989',
+        location: '120 North Sweetzer'
+      }
+    ]
+  },
+  {
+    name: 'Beverly Hills',
+    population: 34109,
+    murders: [
+      {
+        name: 'The Ned Dohenny Greystone Mansion Murder',
+        year: '1929',
+        location: '905 Loma Vista Drive'
+      },
+      {
+        name: 'Clara Bow',
+        year: '1930',
+        location: '512 North Bedford Drive'
+      },
+      {
+        name: 'Bugsy Siegel',
+        year: '1947',
+        location: '810 North Linden Drive'
+      },
+      {
+        name: 'Johnny Stompanato',
+        year: '1958',
+        location: '730 North Bedford Drive'
+      },
+      {
+        name: 'The Manson Murders',
+        year: '1969',
+        location: '10066 Cielo Drive (Tate), and 3311 Waverly Drive(LaBianca Home, Los Feliz)'
+      },
+      {
+        name: 'Rex DeGeorge',
+        year: '1970-99',
+        location: 'office in Beverly Hills'
+      },
+      {
+        name: 'Samantha Jane Gailey',
+        year: '1977',
+        location: '12850 Mulholland Drive'
+      },
+      {
+        name: 'The Billionaire Boys Club',
+        year: '1983-84',
+        location: 'Beverly Hills'
+      },
+      {
+        name: 'The Menendez Brothers',
+        year: '1989',
+        location: '722 North Elm Drive'
+      },
+      {
+        name: 'Dag Drollet',
+        year: '1990',
+        location: '2840 North Beverly Drive'
+      },
+      {
+        name: 'Heidi Fleiss',
+        year: '1990-93',
+        location: '1270 Tower Grove Drive'
+      },
+      {
+        name: 'The Beverly Hills Loan Sharks',
+        year: '1994-2000',
+        location: '350 North Canon Drive',
+      },
+      {
+        name: 'Anand Jon Alexander',
+        year: '2002-2007',
+        location: 'Beverly Hills, and New York City',
+      },
+      {
+        name: 'Ronni Chasen',
+        year: '2010',
+        location: 'Sunset and Whittier'
+      }
+    ]
+  },
+  {
+    name: 'Brentwood',
+    population: 2069,
+    murders: [
+      {
+        name: 'Nicole Brown Simpson & Ron Goldman',
+        year: '1994',
+        location: '879 South Bundy Drive'
+      }
+    ]
+  },
+  {
+    name: 'Bronson Canyon, Griffith Park',
+    population: 28836,
+    murders: [
+      {
+        name: 'Hervey Medellin',
+        year: '2011',
+        location: 'Trail by the Hollywood sign'
+      }
+    ]
+  },
+  {
+    name: 'Burbank',
+    population: 104834,
+    murders: [
+      {
+        name: 'Mabel Monohan',
+        city: 'Burbank',
+        population: 104834,
+        year: '1953',
+        location: '1718 Parkside Avenue'
+      }
+    ]
+  },
+  {
+    name: 'Chinatown',
+    population: 17888,
+    murders: [
+      {
+        name: 'Haing Ngor',
+        year: '1996',
+        location: '900 block of North Beaudry Avenue'
+      }
+    ]
+  },
+  {
+    name: 'Coldwater Canyon',
+    population: 11569,
+    murders: [
+      {
+        name: 'Lisa Ann Mather',
+        year: '1985',
+        location: 'Coldwater Canyon'
+      }
+    ]
+  },
+  {
+    name: 'Commerce',
+    population: 12947,
+    murders: [
+      {
+        name: 'José Gallardo Diaz',
+        city: 'Commerce',
+        population: 12947,
+        year: '1942',
+        location: 'Somewhere around 5500 Slauson Avenue'
+      },
+    ]
+  },
+  {
+    name: 'Compton',
+    population: 97612,
+    murders: [
+      {
+        name: 'Terry Carter',
+        year: '2015',
+        location: 'Tam’s Burgers at East Central and Rosecrans in Compton'
+      }
+    ]
+  },
+  {
+    name: 'Covina/West Covina',
+    population: 48462,
+    murders: [
+      {
+        name: 'The Santa Claus Massacre',
+        city: 'Covina/West Covina',
+        population: 48462,
+        year: '2008',
+        location: '1129 East Knollcrest Drive, Covina'
+      },
+      {
+        name: 'Barbara Finch',
+        city: 'Covina/West Covina',
+        population: 48462,
+        year: '1959',
+        location: '2750 Lark Hill Drive, West Covina'
+      }
+    ]
+  },
+  {
+    name: 'Crenshaw',
+    population: 11178,
+    murders: [
+      {
+        name: 'The Black Dahlia',
+        year: '1947',
+        location: 'Norton Avenue'
+      }
+    ]
+  },
+  {
+    name: 'Downtown',
+    population: 6998,
+    murders: [
+      {
+        name: 'The Chinese Massacre of 1871',
+        year: '1871',
+        location: 'Downtown'
+      },
+      {
+        name: 'The Skid Row Slasher',
+        year: '1964-65',
+        location: 'Downtown and Hollywood'
+      },
+      {
+        name: 'Baca & Tanaka',
+        year: '2011',
+        location: '211 West Temple Street (LASD HQ)'
+      },
+      {
+        name: 'Elisa Lam',
+        year: '2013',
+        location: '640 South Main Street'
+      }
+    ]
+  },
+  {
+    name: 'El Monte',
+    population: 116109,
+    murders: [
+      {
+        name: 'Geneva Hilliker Ellroy',
+        year: '1958',
+        location: 'Arroyo High School, near the athletic fields'
+      }
+    ]
+  },
+  {
+    name: 'Encino',
+    population: 44581,
+    murders: [
+      {
+        name: 'Phil Hartman Murder/Suicide',
+        year: '1998',
+        location: '5065 Encino Avenue'
+      },
+    ]
+  },
+  {
+    name: 'Fairfax District',
+    population: 7598,
+    murders: [
+      {
+        name: 'Laurence Austin',
+        year: '1997',
+        location: '611 North Fairfax Avenue'
+      }
+    ]
+  },
+  {
+    name: 'Freeway Killers',
+    population: 3792621,
+    murders: [
+      {
+        name: 'Trash Bag Murderer',
+        year: '1962-1977',
+        location: 'Freeways'
+      },
+      {
+        name: 'William Bonin',
+        city: 'Freeway Killers',
+        location: 'Freeways'
+      },
+      {
+        name: 'The Scorecard Killer',
+        year: '1971-1983',
+        location: 'Freeways'
+      }
+    ]
+  },
+  {
+    name: 'Glendale',
+    population: 203054,
+    murders: [
+      {
+        name: 'Evelyn Wright & John Kimmel',
+        year: '1934',
+        location: '1830 Verdugo Vista Drive'
+      },
+      {
+        name: 'The Hillside Strangler',
+        year: '1977-78',
+        location: '703 East Colorado Street'
+      },
+      {
+        name: 'Angel of Death',
+        year: '1989-1997',
+        location: '1509 Wilson Terrace'
+      }
+    ]
+  },
+  {
+    name: 'Hollywood',
+    population: 11329,
+    murders: [
+      {
+        name: 'Jerome Ward',
+        year: '1940',
+        location: '1438 North Gower Street'
+      },
+      {
+        name: 'The Hole-in-the-Ground Gang',
+        year: '1986-1987',
+        location: 'First Interstate Bank at Spaulding Avenue and Sunset, followed by Bank of America at Pico and La Cienega, and Union Federal Savings and Loan on Wilshire Boulevard in Beverly Hills'
+      },
+      {
+        name: 'The Gray Widows',
+        year: '1999-2005',
+        location: '307 North La Brea Avenue'
+      },
+      {
+        name: 'Harry Burkhart',
+        year: '2011',
+        location: 'Hollywood, Westside, the Valley'
+      }
+    ]
+  },
+  {
+    name: 'Hollywood Hills',
+    population: 5311,
+    murders: [
+      {
+        name: 'Nick Gianaclis',
+        year: '1947',
+        location: '8444 Magnolia Drive',
+      },
+      {
+        name: 'The Bling Ring',
+        year: '2008-2009',
+        location: 'Hollywood Hills',
+      },
+    ]
+  },
+  {
+    name: 'Indio',
+    population: 89793,
+    murders: [
+      {
+        name: 'The Lonely Hearts Killer',
+        year: '1957-58',
+        location: 'The desert near Indio, and Anza State Park '
+      }
+    ]
+  },
+  {
+    name: 'Koreatown',
+    population: 41416,
+    murders: [
+      {
+        name: 'Louise Peete',
+        year: '1920-44',
+        location: '675 Catalina Street '
+      },
+      {
+        name: 'The Fox',
+        year: '1927',
+        location: 'Manhattan Place and 5th Street'
+      },
+      {
+        name: 'The RFK Assassination',
+        year: '1968',
+        location: '3400 Wilshire'
+      }
+    ]
+  },
+  {
+    name: 'La Canada Flintridge',
+    population: 20413,
+    murders: [
+      {
+        name: 'Mary Busch',
+        year: '1935',
+        location: 'La Canada Flintridge'
+      }
+    ]
+  },
+  {
+    name: 'Laurel Canyon',
+    population: 3020,
+    murders: [
+      {
+        name: 'Ramon Novarro',
+        year: '1968',
+        location: '3110 Laurel Canyon Boulevard'
+      },
+      {
+        name: 'The Wonderland Murders',
+        year: '1981',
+        location: '8763 Wonderland Avenue'
+      },
+    ]
+  },
+  {
+    name: 'Little Armenia, East Hollywood',
+    population: 73967,
+    murders: [
+      {
+        name: 'Juro Rokumoto',
+        year: '1929',
+        location: 'Sunset and Serrano'
+      }
+    ]
+  },
+  {
+    name: 'Los Angeles (various locations)',
+    population: 9818605,
+    murders: [
+      {
+        name: 'The Westside Rapist',
+        year: '1972-89',
+        location: 'multiple'
+      },
+      {
+        name: 'Golden State Killer / East Area Rapist',
+        year: '1979-86',
+        location: 'multiple'
+      },
+      {
+        name: 'The Night Stalker',
+        year: '1984-85',
+        location: 'multiple'
+      }
+    ]
+  },
+  {
+    name: 'Los Feliz',
+    population: 35238,
+    murders: [
+      {
+        name: 'The Los Feliz Murder House',
+        year: '1959',
+        location: '2475 Glendower Place'
+      },
+      {
+        name: 'The Manson Murders',
+        year: '1969',
+        location: 'LaBianca home, 3311 Waverly Drive, and 10066 Cielo Drive(Beverly Hills - Tate home)'
+      },
+      {
+        name: 'Johnny Lewis',
+        year: '2012',
+        location: '3605 Lowry'
+      },
+    ]
+  },
+  {
+    name: 'MacArthur Park, Westlake',
+    population: 103839,
+    murders: [
+      {
+        name: 'Chloe Canfield',
+        year: '1906',
+        location: '8th and Alvarado'
+      },
+      {
+        name: 'William Desmond Taylor',
+        year: '1922',
+        location: 'Maryland and Alvarado'
+      }
+    ]
+  },
+  {
+    name: 'Malibu',
+    population: 12877,
+    murders: [
+      {
+        name: 'Mitrice Richardson',
+        year: '2010',
+        location: 'Malibu Lost Hills Sheriff\'s Station/Dark Canyon'
+      }
+    ]
+  },
+  {
+    name: 'Miracle Mile, Mid-Wilshire',
+    population: 41683,
+    murders: [
+      {
+        name: 'Biggie Smalls (Christopher Wallace)',
+        year: '1997',
+        location: 'Fairfax and Wilshire'
+      }
+    ]
+  },
+  {
+    name: 'Montecito Heights',
+    population: 16768,
+    murders: [
+      {
+        name: 'Tiger Girl',
+        year: '1922',
+        location: 'Montecito Drive',
+      },
+    ]
+  },
+  {
+    name: 'Newport Harbor',
+    population: 86160,
+    murders: [
+      {
+        name: 'Beulah Louise Overell and George (Bud) Gollum',
+        year: '1947',
+        location: 'Newport Harbor'
+      }
+    ]
+  },
+  {
+    name: 'North Hollywood',
+    population: 74671,
+    murders: [
+      {
+        name: 'North Hollywood Shootout',
+        year: '1997',
+        location: '6600 Laurel Canyon Boulevard',
+      },
+    ]
+  },
+  {
+    name: 'Northeast Los Angeles',
+    population: 167674,
+    murders: [
+      {
+        name: 'Timothy Joseph McGhee',
+        year: '1997-2001',
+        location: 'Northeast Los Angeles'
+      }
+    ]
+  },
+  {
+    name: 'Norwalk',
+    population: 106084,
+    murders: [
+      {
+        name: 'Stella Darlene Nolan',
+        year: '1953',
+        location: '10900 East Firestone Boulevard'
+      }
+    ]
+  },
+  {
+    name: 'Orange County, Los Angeles, and Other States',
+    population: 3010232,
+    murders: [
+      {
+        name: 'The “Dating Game” Killer',
+        year: '1968-79',
+        location: 'various'
+      }
+    ]
+  },
+  {
+    name: 'Pacific Palisades',
+    population: 24651,
+    murders: [
+      {
+        name: 'Thelma Todd',
+        year: '1935',
+        location: '17575 Pacific Coast Highway'
+      }
+    ]
+  },
+  {
+    name: 'Reseda',
+    population: 79687,
+    murders: [
+      {
+        name: 'ZZZZ Best Carpet Cleaning',
+        city: 'Reseda',
+        population: 79687,
+        year: '1982-87',
+        location: '7040 Darby Avenue'
+      }
+    ]
+  },
+  {
+    name: 'Riverside',
+    population: 327728,
+    murders: [
+      {
+        name: 'The Chicken Coop Killer',
+        year: '1920s',
+        location: '6330 Wineville Road, Mira Loma'
+      }
+    ]
+  },
+  {
+    name: 'San Diego',
+    population: 3095313,
+    murders: [
+      {
+        name: 'Dan and Linda Kolkena Broderick',
+        year: '1989',
+        location: '1041 Cypress Avenue, Marston Hills'
+      }
+    ]
+  },
+  {
+    name: 'San Gabriel Mountains',
+    population: 1510378,
+    murders: [
+      {
+        name: 'Jon Aujay',
+        year: '1998',
+        location: 'Devil\'s Punchbowl'
+      }
+    ]
+  },
+  {
+    name: 'San Pedro',
+    population: 80065,
+    murders: [
+      {
+        name: 'Doris Courtemanche',
+        year: '1943',
+        location: '751 North Palos Verdes Street'
+      }
+    ]
+  },
+  {
+    name: 'Santa Monica',
+    population: 92306,
+    murders: [
+      {
+        name: 'Col. Griffith J. Griffith',
+        year: '1903',
+        location: 'Ocean Avenue'
+      },
+      {
+        name: 'Whitney Bulger',
+        year: '1997-2011',
+        location: '1012 3rd Street #303'
+      }
+    ]
+  },
+  {
+    name: 'Sherman Oaks',
+    population: 69398,
+    murders: [
+      {
+        name: 'Linda Epping',
+        year: '1961',
+        location: 'Newspaper reports say Phillips worked in in Sherman Oaks'
+      }
+    ]
+  },
+  {
+    name: 'Silver Lake',
+    population: 30972,
+    murders: [
+      {
+        name: 'The “Bat Man” Case',
+        year: '1922',
+        location: '800 block of Lafayette Park Place in Silver Lake'
+      }
+    ]
+  },
+  {
+    name: 'Simi Valley',
+    population: 126878,
+    murders: [
+      {
+        name: 'Randy Kling',
+        year: '2003-04',
+        location: '2243 East Avenue I, in Lancaster; and Rio Vista Court in Simi Valley'
+      }
+    ]
+  },
+  {
+    name: 'South Los Angeles',
+    population: 271040,
+    murders: [
+      {
+        name: 'Sam Cooke',
+        year: '1964',
+        location: '9137 South Figueroa Street'
+      },
+      {
+        name: 'The Grim Sleeper',
+        year: '1985-2007',
+        location: 'South Los Angeles'
+      },
+      {
+        name: 'Latasha Harlins',
+        year: '1992',
+        location: 'Empire Liquor Market and Deli, 91st and Figueroa streets'
+      },
+      {
+        name: 'George "G" Torres',
+        year: '2001',
+        location: 'Vitello’s at 4349 Tujunga Avenue'
+      }
+    ]
+  },
+  {
+    name: 'Studio City',
+    population: 36636,
+    murders: [
+      {
+        name: 'Diane Sparks',
+        year: '1946',
+        location: 'Olive orchard a half-mile off Glenoaks Boulevard'
+      }
+    ]
+  },
+  {
+    name: 'Van Nuys',
+    population: 102357,
+    murders: [
+      {
+        name: 'Sherri Rasmussen',
+        year: '1986',
+        location: 'Townhouse in a gated complex on the 7100 block of Balboa Boulevard'
+      }
+    ]
+  },
+  {
+    name: 'Ventura',
+    population: 110790,
+    murders: [
+      {
+        name: 'Olga Kupczyk Duncan',
+        year: '1958',
+        location: 'Casitas Pass in Ventura'
+      }
+    ]
+  },
+  {
+    name: 'West Adams',
+    population: 46303,
+    murders: [
+      {
+        name: 'Marvin Gaye',
+        year: '1984',
+        location: '2101 South Gramercy Place'
+      }
+    ]
+  },
+  {
+    name: 'West Hills',
+    population: 38814,
+    murders: [
+      {
+        name: 'Clyde and Lulu Dayton',
+        year: '1922',
+        location: 'Their ranch extended from Roscoe and Valley Circle to the Ventura County line'
+      },
+      {
+        name: 'Jesse James Hollywood',
+        year: '2000',
+        location: 'Lizard\'s Mouth hiking area'
+      }
+    ]
+  },
+  {
+    name: 'West Hollywood',
+    population: 37080,
+    murders: [
+      {
+        name: 'Sal Mineo',
+        year: '1976',
+        location: '8563 Holloway Drive'
+      },
+      {
+        name: 'Christa Helm',
+        year: '1977',
+        location: 'LLoyd Place'
+      },
+      {
+        name: 'Dominique Dunne',
+        year: '1982',
+        location: '8723 Rangely Avenue'
+      }
+    ]
+  },
+  {
+    name: 'West Los Angeles',
+    population: 529427,
+    murders: [
+      {
+        name: 'Melanie Howell',
+        year: '1976',
+        location: '1723 Westgate Avenue #6'
+      },
+      {
+        name: 'Dorothy Stratten',
+        year: '1980',
+        location: '10881 Clarkson Road'
+      }
+    ]
+  },
+  {
+    name: 'Westwood',
+    population: 47916,
+    murders: [
+      {
+        name: 'Karen Toshima',
+        year: '1988',
+        location: '1000 block of Broxton Avenue'
+      }
+    ]
+  }
+]
+
+module.exports = murderData;
