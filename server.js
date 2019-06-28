9735b5778f5a1d16a7d289ec14aa687b6148ef2b
raechel <odom.raechel@gmail.com>
2019-06-28 10:03:18 -0600

    deploy

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
+const express = require('express');
+const app = express();
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
-const express = require('express');
-const app = express();
-const port = 3000;
+
+app.set('port', process.env.PORT || 3000);
 
 app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`App is running on port ${port} 🚀`);
+app.listen(app.get('port'), () => {
+  console.log(`App is running 🚀`);
+});
+
+app.get('/', (req, res) => {
+  res.status(200).json('Hello world!');
 });
 
 app.get('/api/v1/crimes', (req, res) => {
